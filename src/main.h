ccd88039584035bea8ef831b78a3f9954a2544c1
DOLLAR ONLINE <info@edollar.online>
2016-11-21 22:30:45 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -39,7 +39,7 @@ static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 1000000000 * COIN;
 static const int64_t COIN_YEAR_REWARD = 20 * CENT; // 20% per year
 static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.2 * COIN;	// 20% annual interest
-static const int MODIFIER_INTERVAL_SWITCH = 1000;
+static const int MODIFIER_INTERVAL_SWITCH = 2000;
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
